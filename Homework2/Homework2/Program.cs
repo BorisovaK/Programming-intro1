1b34f604d8323997a02ffcc0ec1bc219c30565f3
lucifer666 <philip_genev@abv.bg>
2015-10-18 21:46:26 +0300

    This is my Homework 'Hello world'

---
+++ Homework2/Homework2/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Homework2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World, my name is Katrin and this is my first program!");
+            Console.WriteLine("I hope that this is not my last program");
+            Console.Read();
+            
+        }
+    }
+}
